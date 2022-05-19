9e8df6ecdb8d23d8bf4c6ede6fd86e6feb9ecad4
NicoHeinola <69452295+NicoHeinola@users.noreply.github.com>
2022-05-19 08:56:44 +0300

    php done

--- pikataival/10/index.php
+++ pikataival/10/index.php
@@ -1,2 +1,16 @@
 <!-- Toteuta tähän tarvittava koodi -->
 
+<?php
+$arr = [];
+
+for($i = 1; $i < 101; $i++){
+    array_push($arr,$i);
+}
+
+shuffle($arr);
+
+echo $arr[0];
+echo $arr[1];
+echo $arr[2];
+echo $arr[3];
+echo $arr[4];
--- pikataival/9/index.php
+++ pikataival/9/index.php
@@ -1,1 +1,9 @@
 <!-- Toteuta tähän tarvittava koodi -->
+<?php
+
+for($i = 1; $i < 11; $i++){
+    echo $i;
+    if($i < 10){
+        echo "-";
+    }
+}
